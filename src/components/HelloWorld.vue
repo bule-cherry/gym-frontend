3d64b2f33b71f1e38f66a774a901296fe2a05b2e
cheery6103 <3141988275@qq.com>
2025-10-26 11:02:57 +0800

    引入element plus

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
+import { ElBacktop } from 'element-plus';
+
 
 </script>
 
 <template>
   <div> 这是一个helloworld组件 </div>
+  <el-form-item>
+    <el-button type="primary" native-type="submit">按钮</el-button>
+  </el-form-item>
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,15 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from '@/router/index'
+//引入 element plus 环境
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
-app.use(router)
+app.use(router).use(ElementPlus).mount('#app')
 
-app.mount('#app')
+//全局注册图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+ }
