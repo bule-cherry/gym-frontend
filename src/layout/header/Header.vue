d34bb45d551a070a74117bbb82ac3c517a34c9b3
cheery6103 <3141988275@qq.com>
2025-10-26 17:09:27 +0800

    面包屑导航制作

--- src/layout/header/Header.vue
+++ src/layout/header/Header.vue
@@ -1,9 +1,11 @@
 <template>
     <collapse></collapse>
+    <bread-crumb></bread-crumb>
 </template>
 
 <script lang="ts" setup>
 import Collapse from './Collapse.vue';
+import BreadCrumb from './BreadCrumb.vue';
 </script>
 
 <style>
