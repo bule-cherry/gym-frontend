f9c5ad385353a957c26d12b0958eccd954a3ca97
cheery6103 <3141988275@qq.com>
2025-10-26 20:08:19 +0800

    弹框组件封装

--- src/views/system/UserList.vue
+++ src/views/system/UserList.vue
@@ -1,9 +1,28 @@
 <template>
-    <h1>员工管理</h1>
+    <div>
+        <el-button type="primary" size="default" @click="addBtn">新增</el-button>
+        <!-- 传入 dialog 的所有属性 -->
+        <sys-dialog 
+        :title="dialog.title"
+        :visible="dialog.visible"
+        :width="dialog.width"
+        :height="dialog.height"
+        @onClose="onClose"
+        @onConfirm="onConfirm"
+        >
+            <template v-slot:content>
+                <div>我是弹框</div>
+            </template>
+        </sys-dialog>
+    </div>
 </template>
 <script setup lang="ts">
-
+import SysDialog from "@/components/SysDialog.vue";
+import useDialog from "@/hooks/useDialog";
+//弹框属性
+const {dialog,onClose,onConfirm} = useDialog()
+const addBtn = () => {
+    dialog.visible = true;
+};
 </script>
-<style lang="scss">
-
-</style>
+<style scoped></style>
