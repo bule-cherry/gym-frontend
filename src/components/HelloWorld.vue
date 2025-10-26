f4301c21f4cfe57cbd673a34bc1e94f834be884d
cheery6103 <3141988275@qq.com>
2025-10-26 11:20:35 +0800

    引入pinia

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,19 @@
-<script setup lang="ts">
-import { ElBacktop } from 'element-plus';
-
-
-</script>
-
 <template>
   <div> 这是一个helloworld组件 </div>
   <el-form-item>
-    <el-button type="primary" native-type="submit">按钮</el-button>
+    <el-button type="primary" native-type="submit" @click="addCount">按钮</el-button>
+    <h3>{{ store.count }}</h3>
   </el-form-item>
 </template>
+
+<script setup lang="ts">
+import { testStore } from '@/store/test';
+const store = testStore()
+const addCount = ()=>{
+  store.count++
+}
+
+</script>
 
 <style scoped>
 
--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,12 @@ import router from '@/router/index'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+//引入pinia 的环境
+import { createPinia } from 'pinia'
+const pinia = createPinia()
 
 const app = createApp(App)
-app.use(router).use(ElementPlus).mount('#app')
+app.use(router).use(ElementPlus).use(pinia).mount('#app')
 
 //全局注册图标
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
